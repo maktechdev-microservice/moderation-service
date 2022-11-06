d40fd98616788d6abcd7de4fcd63960d2cbb1b16
maktechdev <39600404+maktechdev@users.noreply.github.com>
2022-11-06 11:30:44 +0100

    Adds major reformating

--- server.js
+++ server.js
@@ -1,37 +1,44 @@
-import express from "express"
-import axios from "axios"
-import bodyParser from "body-parser"
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
 
-const app = express()
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.json());
 
-
-const port = 4003
-
+const port = 4003;
 
 app.post("/events", async (req, res) => {
-    console.log(`get ${req.body}`)
-    const { type, data: { id, content, postId } } = req.body
-    setTimeout( async () => {
-        if (type==="CommentCreated") {
-        const status = (content.includes('wtf')) ? "rejected" : "approved"
-        await axios.post("http://events-clusterip-srv:4005/events", {
-            type: "CommentModerated",
-            data: {
-                id, content, postId, status
-            }
-        }).catch(err => {
-            console.log(`moderator reports ${err}`)
-        })
-    }
-    }, 25000);
-    
-    res.send({})
-})
+  const {
+    type,
+    data: { id, content, postId },
+  } = req.body;
+  console.log(`received ${type}, ${content}, ${postId}`);
+  if (type === "CommentCreated") {
+    const status = content.includes("wtf") ? "rejected" : "approved";
+    // posting the updated comment to event-bus
+    await axios
+      .post("http://events-clusterip-srv:4005/events", {
+        type: "CommentModerated",
+        data: {
+          id,
+          content,
+          postId,
+          status,
+        },
+      })
+      .catch((err) => {
+        console.log(`moderator reports ${err}`);
+      });
+  }
 
+
+  res.send({})
+});
 
 app.listen(port, async () => {
-    console.log(`Moderator service is listening at ${port}`)
-    const resp = await axios.get('http://events-clusterip-srv:4005/events').catch(err => console.log(`text ${err}`))
-    console.log(`<data:></data:> ${resp.data}`)
-})
+  console.log(`Moderator service is listening at ${port}`);
+  const resp = await axios
+    .get("http://events-clusterip-srv:4005/events")
+    .catch((err) => console.log(`text ${err}`));
+  console.log(`moderator after restart: ${resp.data}`);
+});
